796396121f7e1f5661316ce6f70b6f32285378b6
Alaa Hudhud <malakbawab29@gmail.com>
2021-01-07 12:14:13 +0200

    Second Commit

--- src/test/java/com/exalt/assignment/CharsCountTest.java
+++ src/test/java/com/exalt/assignment/CharsCountTest.java
@@ -4,8 +4,6 @@ import org.junit.*;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
  * JUnit tests to verify logic implemented to count chars occurrences
@@ -14,6 +12,7 @@ import java.util.TimerTask;
 public class CharsCountTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private ClassLoader loader = CharsCountTest.class.getClassLoader();
+
     @Rule
     public TemporaryFolder tempFolder = new TemporaryFolder();
 
@@ -26,10 +25,11 @@ public class CharsCountTest {
     }
 
     /**
-     * This test verify the occurrences of lower-case latin chars in the latin folder by comparing the actual,expected condole output,
-     * latin folder contains 5 testcase files and 2 subfolder(one of them has another subfolder inside it).
-     * each subfolder also contain 5 testcase files.
-     * The content of testcase file is the string "abcd" written 3 times across lines.
+     * This test verify the occurrences of lower-case latin chars in latin folder(in resources)
+     * by comparing the actual,expected console output,latin folder contains 5 testcase
+     * files and 2 subfolder(one of them has another subfolder inside it). Each subfolder
+     * also contains 5 testcase files.The content of each testcase file is a string "abcd"
+     * written 3 times across lines.
      *
      * @throws Exception
      */
@@ -66,40 +66,6 @@ public class CharsCountTest {
 
         StringBuilder expectedOutput = new StringBuilder();
         for (int i = 97; i <= 122; i++) {
-            Character character = (char) (i);
-            expectedOutput.append(character).append("\t \t").append("0").append("\n");
-        }
-        Assert.assertEquals(expectedOutput.toString(), outContent.toString());
-    }
-
-    @Test
-    public void testLargeFile() throws Exception {
-
-        File createdFile = tempFolder.newFile("case_1");
-        new java.util.Timer().schedule(new TimerTask() {
-            @Override
-            public void run() {
-                FileWriter fw = null;
-                try {
-                    fw = new FileWriter(createdFile);
-                    fw.append("abcd");
-                    fw.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        },0, 300000);
-
-        String[] arguments = {tempFolder.getRoot().getPath()};
-        CharsCount.main(arguments);
-
-        StringBuilder expectedOutput = new StringBuilder();
-        expectedOutput.append('a' + "\t \t" + "2" + "\n");
-        expectedOutput.append('b' + "\t \t" + "1" + "\n");
-        expectedOutput.append('c' + "\t \t" + "1" + "\n");
-        expectedOutput.append('d' + "\t \t" + "1" + "\n");
-
-        for (int i = 101; i <= 122; i++) {
             Character character = (char) (i);
             expectedOutput.append(character).append("\t \t").append("0").append("\n");
         }
